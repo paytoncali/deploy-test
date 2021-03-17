cb3b57b398f46675192676c873cf2d3db4b21e04
Payton Whinnery <paytoncaliw@gmail.com>
2021-03-17 16:45:14 -0500

    fixing issues

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
     return fs.readFile(path.join(__dirname, "index.html"), (e, data) => {
         if (e) console.error(e);
         res.writeHead(200, {'Content-Type': "text/html"});
+        res.end(data);
         });
 });
 
